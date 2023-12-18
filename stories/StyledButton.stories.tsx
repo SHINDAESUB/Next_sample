4d2c2c01d152ee65987f670c15d86e635eca91cd
SHINDAESUB <weotjqw@naver.com>
2023-12-18 15:23:03 +0900

    feat: 컨트롤 탭을 사용한 props 제어

--- stories/StyledButton.stories.tsx
+++ stories/StyledButton.stories.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ComponentMeta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { StyledButton } from '../components/StyledButton'
 import { action } from '@storybook/addon-actions';
 
@@ -9,7 +9,30 @@ export default {
     title: 'StyledButton',
     // 사용하는 컴포넌트
     components: StyledButton,
+    argTypes:{
+      // prop에 전달하는 variant를 스토리북으로부터 변경할 수 있도록 추가
+      variant:{
+        control: { type: 'radio'},
+        options: ['primary', 'success', 'transparent'],
+      },
+      // props에 전달하는 children을 스토리북으로부터 변경할 수 있도록 추가
+      children: {
+          //텍스트 박스에 입력할 수 있도록 지정
+          control: { type: 'text' }
+      },
+    },
 } as ComponentMeta<typeof StyledButton>
+
+const Template: ComponentStory<typeof StyledButton> = (args) =>
+    <StyledButton {...args} />
+
+export const TemplateTest = Template.bind({});
+
+// 기본 props를 설정한다
+TemplateTest.args = {
+    variant: 'primary',
+    children: 'primary'
+}
 
 // increment라는 이름으로 action을 출력하기 위한 함수를 만든다
 const incrementAction = action('increment');
